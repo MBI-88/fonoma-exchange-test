83467b656dc60ae53b9fb7a37fff5961b90a8833
MBI-88 <ingmbi8807@gmail.com>
2023-06-04 18:47:07 -0300

    change to service

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,12 @@
 import Form from "./components/form";
-import currencies from './database/currencies.json'
-import { Currencies } from "./interfaces/currencies";
+import GetCurrencies from "./services/home.currencies";
 
-const url = process.env.BASE_URL
-const apikey = process.env.APIKEY
-
-async function getData() {
-  const query = `${url}currencies?apikey=${apikey}&currencies`
-  const res = await fetch(query, { next: { revalidate: 86400 } })
-  if (!res.ok) {
-    return currencies
-  }
-  return res.json()
-}
 
 export default async function Home() {
-  const res = await getData()
-  const data:Currencies = {...res}
-  return <main className="min-h-screen"><Form data={data} /></main>
-  
+  const data = await GetCurrencies()
+  return (
+    <main className="min-h-screen">
+      <Form data={data} />
+    </main>
+  )
 }
--- app/services/home.currencies.ts
+++ app/services/home.currencies.ts
@@ -1,7 +1,14 @@
 import { Currencies } from "../interfaces/currencies";
+import currencies from '../database/currencies.json'
+
+const url = process.env.BASE_URL
+const apikey = process.env.APIKEY
 
 export default async function GetCurrencies() {
-        const response = await fetch("/api/currencies")
-        const data:Currencies = await response.json()
-        return data
+        const query = `${url}currencies?apikey=${apikey}&currencies`
+        const res = await fetch(query, { next: { revalidate: 86400 } })
+        if (!res.ok) {
+                return currencies
+        }
+        return res.json() 
 }
