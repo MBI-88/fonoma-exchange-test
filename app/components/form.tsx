db744f34d45bd4e7b7e44f6cd4851997e713b744
MBI-88 <ingmbi8807@gmail.com>
2023-06-04 18:18:44 -0300

    setting input number

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -62,7 +62,7 @@ const Form: FC<Props> = ({ data }) => {
                     </div>
                     <div className="mb-6">
                         <label htmlFor="value" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount in {select.from}</label>
-                        <input value={input.currency} onChange={handleInput} name="currency" type="text" id="value" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0" required />
+                        <input value={input.currency} onChange={handleInput} name="currency" type="number" id="value" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0" required />
                     </div>
                     <div className="mb-6">
                         <label htmlFor="result" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Result</label>
